54313ff957dd0825e47c77ff00625deb8438b07e
Ievgen Gavrysh <igavrysh@gmail.com>
2016-06-01 19:33:16 +0300

    initial commit

---
+++ SuperObjCProject/Sources/IDPBuilding/IDPBuilding.m
@@ -0,0 +1,13 @@
+//
+//  IDPBuilding.m
+//  SuperObjCProject
+//
+//  Created by Ievgen on 6/1/16.
+//  Copyright © 2016 1mlndollarsasset. All rights reserved.
+//
+
+#import "IDPBuilding.h"
+
+@implementation IDPBuilding
+
+@end
