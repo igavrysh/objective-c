53e4670ccf89ec9c0276e1f7b41a91ea50adc6f3
Ievgen <zhenya.ga@gmail.com>
2016-06-16 14:58:10 +0300

    add addObject to IDPBinaryTreeSet

--- SuperObjCProject/Shared/IDPSet/Private/IDPBinaryTreeSet.m
+++ SuperObjCProject/Shared/IDPSet/Private/IDPBinaryTreeSet.m
@@ -13,8 +13,33 @@
 @interface IDPBinaryTreeSet ()
 @property (nonatomic, retain) IDPBinaryTreeNode *root;
 
+- (void)addObject:(IDPComparisonId)object;
+- (void)addObject:(IDPComparisonId)object;
 @end
 
 @implementation IDPBinaryTreeSet
+
+#pragma mark -
+#pragma mark Public Methods
+
+- (void)addObject:(IDPComparisonId)object {
+  [self addObject:object node:&self.root];
+}
+
+#pragma mark -
+#pragma mark Private Methods
+
+- (void)addObject:(IDPComparisonId)object node:(IDPBinaryTreeNode **)node {
+  if (!*node) {
+    *node = [[[IDPBinaryTreeNode alloc] initWithObject:object] autorelease];
+  } else if (![*node isFull]){
+    [*node addObject:object];
+  } else if (![*node.leftChild isFull]) {
+    [self addObject: object node:&node.leftChild];
+  } else {
+    [self addObject: object node:&node.rightChild];
+  }
+}
+
 
 @end
